024bda580658637afed461bbc13aa3677e4080d1
xiaosongfu <xiaosong.fu@outlook.com>
2023-09-26 22:48:14 +0800

    add new version

--- README.md
+++ README.md
@@ -2,4 +2,36 @@
 
 SNS JS is the JavaScript API wrapper for [SeeDAO Name Service](https://github.com/Taoist-Labs/sns).
 
-[Read Docs](https://taoist-labs.github.io/sns-js/)
+## APIs
+
+## 1.1 Resolve addr from sns
+
+```typescript
+import { sns } from "sns-js";
+
+const addr: string = await sns.resolve("abc.seedao")
+```
+
+## 1.2 Resolve sns from addr
+
+```typescript
+import { sns } from "sns-js";
+
+const sns: string = await sns.name("0x123...789")
+```
+
+## 2.1 Batch resolve addr from sns
+
+```typescript
+import { sns } from "sns-js";
+
+const addrArr: string[] = await sns.resolves(["abc.seedao", "def.seedao"]);
+```
+
+## 2.2 Batch resolve sns from addr
+
+```typescript
+import { sns } from "sns-js";
+
+const snsArr: string[] = await sns.names(["0x123...789", "0x456...654"]);
+```
---
+++ src/index.ts
@@ -0,0 +1,121 @@
+import { Contract, JsonRpcProvider, namehash } from "ethers";
+import { Multicall } from "ethereum-multicall";
+import {
+  RPC,
+  PUBLIC_RESOLVER_ABI,
+  PUBLIC_RESOLVER_ADDR,
+  REVERSE_REGISTRAR_ABI,
+  REVERSE_REGISTRAR_ADDR,
+} from "./constant";
+
+export namespace sns {
+  export async function resolve(sns: string, rpc?: string): Promise<string> {
+    return new Contract(
+      PUBLIC_RESOLVER_ADDR,
+      PUBLIC_RESOLVER_ABI,
+      new JsonRpcProvider(rpc ?? RPC),
+    ).addr(namehash(sns));
+  }
+
+  export async function name(addr: string, rpc?: string): Promise<string> {
+    const provider = new JsonRpcProvider(rpc ?? RPC);
+
+    const node = await new Contract(
+      REVERSE_REGISTRAR_ADDR,
+      REVERSE_REGISTRAR_ABI,
+      provider,
+    ).node(addr);
+
+    return new Contract(
+      PUBLIC_RESOLVER_ADDR,
+      PUBLIC_RESOLVER_ABI,
+      provider,
+    ).name(node);
+  }
+
+  export async function resolves(
+    snsArr: string[],
+    rpc?: string,
+  ): Promise<string[]> {
+    // instance a `multicall`
+    const multicall = new Multicall({
+      nodeUrl: rpc ?? RPC,
+      tryAggregate: true,
+    });
+
+    // prepare `calls` parameter
+    const calls = snsArr.map((sns) => {
+      return {
+        reference: "addr",
+        methodName: "addr(bytes32)",
+        methodParameters: [namehash(sns)],
+      };
+    });
+    // execute multi call
+    const r = await multicall.call({
+      reference: "PublicResolver",
+      contractAddress: PUBLIC_RESOLVER_ADDR,
+      abi: PUBLIC_RESOLVER_ABI,
+      calls: calls,
+    });
+
+    // return results
+    return r.results["PublicResolver"].callsReturnContext.map((ctx) => {
+      return ctx.returnValues[0];
+    }) as string[];
+  }
+
+  export async function names(
+    addrArr: string[],
+    rpc?: string,
+  ): Promise<string[]> {
+    // instance a `multicall`
+    const multicall = new Multicall({
+      nodeUrl: rpc ?? RPC,
+      tryAggregate: true,
+    });
+
+    // <----- get `node` from `addr`
+    // prepare `calls` parameter
+    let calls = addrArr.map((addr) => {
+      return {
+        reference: "node",
+        methodName: "node(address)",
+        methodParameters: [addr],
+      };
+    });
+    // execute multi call
+    let results = await multicall.call({
+      reference: "ReverseRegistrar",
+      contractAddress: REVERSE_REGISTRAR_ADDR,
+      abi: REVERSE_REGISTRAR_ABI,
+      calls: calls,
+    });
+    // <----- get `node` from `addr`
+
+    // <----- get `name` from `node`
+    // prepare `calls` parameter
+    calls = results.results["ReverseRegistrar"].callsReturnContext.map(
+      (ctx) => {
+        return {
+          reference: "name",
+          methodName: "name(bytes32)",
+          methodParameters: [ctx.returnValues[0]],
+        };
+      },
+    );
+    // execute multi call
+    results = await multicall.call({
+      reference: "PublicResolver",
+      contractAddress: PUBLIC_RESOLVER_ADDR,
+      abi: PUBLIC_RESOLVER_ABI,
+      calls: calls,
+    });
+    // <----- get `name` from `node`
+
+    // return results
+    return results.results["PublicResolver"].callsReturnContext.map((ctx) => {
+      return ctx.returnValues[0];
+    }) as string[];
+  }
+}
