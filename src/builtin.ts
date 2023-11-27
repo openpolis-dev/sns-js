39b42a0d4ef85f57a3c552ac5b536115fb212319
xiaosongfu <xiaosong.fu@outlook.com>
2023-11-27 17:39:22 +0800

    fix: add `SeeDAORegistrarController` address

--- src/builtin.ts
+++ src/builtin.ts
@@ -12,8 +12,15 @@ export const RPC: string =
 export const PUBLIC_RESOLVER_ADDR: string =
   "0x4ffCfd37C362B415E4c4A607815f5dB6A297Ed8A"; // PublicResolver contract address
 
+export const SEEDAO_REGISTRAR_CONTROLLER_ADDR: string =
+  "0xded0a911F095349A071CA71Bb8237C4a40947159"; // SeeDAORegistrarController contract address
+
+// ---> test environment
+//
+
 // ---> dev environment
 // export const INDEXER_HOST: string = "http://localhost:3000";
 // export const SAFE_HOST: string = "http://localhost:8090";
 // export const RPC: string = "http://127.0.0.1:8545/";
 // export const PUBLIC_RESOLVER_ADDR: string = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
+// export const SEEDAO_REGISTRAR_CONTROLLER_ADDR: string = "0xded0a911F095349A071CA71Bb8237C4a40947159";
