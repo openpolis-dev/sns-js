fd2f4aede2cbe77974fed6c2271054bdd9111829
xiaosongfu <xiaosong.fu@outlook.com>
2023-12-14 21:04:53 +0800

    fix: update contract address

--- src/builtin.ts
+++ src/builtin.ts
@@ -6,19 +6,17 @@ export const SAFE_HOST: string = "https://test-sns-api.seedao.tech";
 
 // RPCs
 export const RPC: string =
-  "https://eth-goerli.g.alchemy.com/v2/MATWeLJN1bEGTjSmtyLedn0i34o1ISLD";
+  "https://polygon-mainnet.g.alchemy.com/v2/-MLinGy2l91vLVZWXmRfNYf9DavMxaEA";
 
 // Deployed Addresses
 export const BASE_REGISTRAR_ADDR: string =
-  "0x4C53Ff1A6a47E7089e1E727f83e7b7aEFCC9796B"; // BaseRegistrar contract address
+  "0x5f3BD0CE4445E96F2D7DCC4bbA883378EAD8E10f"; // BaseRegistrar contract address
 export const SEEDAO_REGISTRAR_CONTROLLER_ADDR: string =
-  "0x78d29889bCa13A477b25933923732FF562DcC7Cd"; // SeeDAORegistrarController contract address
+  "0x0E282cfc8c7293CBfDB80E289E19060C4c85cbED"; // SeeDAORegistrarController contract address
 export const PUBLIC_RESOLVER_ADDR: string =
-  "0x6A80eA63cFfc6B10B764e1f26348832835520646"; // PublicResolver contract address
+  "0x64b7CbacE440808C8d71bb62E80A655331ABe092"; // PublicResolver contract address
 export const SEEDAO_MINTER_ADDR: string =
-  "0x618702B04DC94ACca487aE888Fd986adfC2f66d2"; // SeeDAOMinter contract address
-export const SEEDAO_ACTIVITY_MINTER_ADDR =
-  "0x41B6ECd5BC6Bca0Ca6F9C1c01FfEa6a6b24fF59f"; // SeeDAOActivityMinter contract address
+  "0xd97baE29C72B61142D9D4d148264f31930dFb94F"; // SeeDAOMinter contract address
 
 // ---> dev environment
 // export const INDEXER_HOST: string = "http://localhost:3000";
@@ -27,4 +25,3 @@ export const SEEDAO_ACTIVITY_MINTER_ADDR =
 // export const BASE_REGISTRAR_ADDR: string = "";
 // export const SEEDAO_REGISTRAR_CONTROLLER_ADDR: string = "";
 // export const PUBLIC_RESOLVER_ADDR: string = "";
-// export const SEEDAO_MINTER_ADDR: string = "";
